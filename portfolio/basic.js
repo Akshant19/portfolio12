a67440e7a1fd73b1d993f1ad742c936538d0e7d9
Akshant19 <127925254+Akshant19@users.noreply.github.com>
2024-02-05 16:52:29 +0530

    Add files via upload

---
+++ portfolio/basic.js
@@ -0,0 +1,20 @@
+// const elements = document.querySelectorAll('.heading');
+// elements.forEach(element => {
+//   element.addEventListener('mouseover', () => changeText(element));
+//   element.addEventListener('mouseout', () => resetText(element));
+// });
+
+// function changeText(element) {
+//   element.innerHTML = 'Ashish Hiranwal';
+// }
+
+// function resetText(element) {
+//   element.innerHTML = 'Portfolio';
+// }
+// let id = document.getElementById('heading1');
+    function changeText() {
+        document.getElementById('heading1').innerHTML ='Akshant.';
+      }
+      function resetText() {
+        document.getElementById('heading1').innerHTML = 'Portfolio';
+      }
